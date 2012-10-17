39c1c4ff38a7fa101179bcdda33d91ee7ec9c072
Jerry Peng <pr2jerry@gmail.com>
2012-10-17 23:24:33 +0800

    Enhance LaTeX Exporting
    - Customize latex document class based on scrartcl class
    - Add xeCJK preambles, including font settings
    - Add an advice to org-beamer-amend-header, to add custom headers
      to beamer class

--- emacs/enabled.d/orgmode.el
+++ emacs/enabled.d/orgmode.el
@@ -24,5 +24,79 @@
 
 (setq org-latex-to-pdf-process 
   '("xelatex -interaction nonstopmode %f"
-     "xelatex -interaction nonstopmode %f")) ;; for multiple passes
+    "xelatex -interaction nonstopmode %f")) ;; for multiple passes
+
+(defvar en-article "
+\\documentclass{scrartcl}
+\\usepackage{hyperref}
+\\usepackage{color}
+\\usepackage[hyperref,x11names,usenames,dvipsnames]{xcolor}
+\\hypersetup{colorlinks=true,linkcolor=BlueViolet}
+\\usepackage{listings}
+\\usepackage[top=0.7in,bottom=0.7in,left=0.8in,right=0.8in]{geometry}
+\\usepackage[center,pagestyles]{titlesec}
+\\titleformat{\\section}{\\Large\\bfseries}{\\S\\,\\thesection}{1em}{}
+\\titleformat{\\subsection}{\\large\\bfseries}{\\S\\,\\thesubsection}{1em}{}
+\\titleformat{\\subsubsection}{\\bfseries}{$\\cdot$~\\,\\thesubsubsection}{0.5em}{}
+\\newpagestyle{main}{
+\\sethead{\\small\\S\\,\\thesection\\quad\\sectiontitle}{}{$\\cdot$~\\thepage~$\\cdot$}
+\\setfoot{}{}{}\\headrule}
+\\pagestyle{main}
+")
+
+(defvar zh-preamble "
+\\usepackage{xeCJK}
+\\setCJKmainfont[BoldFont=SimHei, ItalicFont=KaiTi]{SimSun}
+\\setCJKmonofont[Scale=0.9]{Ubuntu Mono}
+\\setCJKfamilyfont{song}[BoldFont=SimSun]{SimSun}
+\\setCJKfamilyfont{sf}[BoldFont=SimSun]{SimSun}
+\\renewcommand{\\contentsname}{目录}
+\\renewcommand{\\listfigurename}{插图目录}
+\\renewcommand{\\listtablename}{表格目录}
+\\renewcommand{\\refname}{参考文献}
+\\renewcommand{\\abstractname}{摘要}
+\\renewcommand{\\indexname}{索引}
+\\renewcommand{\\tablename}{表}
+\\renewcommand{\\figurename}{图}
+")
+
+
+
+(defvar cn-article
+  (concat en-article zh-preamble))
+
+(require 'org-latex)
+(unless (boundp 'org-export-latex-classes)
+  (setq org-export-latex-classes nil))
+
+(add-to-list 'org-export-latex-classes
+             `("article"
+               ,en-article
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+(add-to-list 'org-export-latex-classes
+              `("cn-article"
+                ,cn-article
+                ("\\section{%s}" . "\\section*{%s}")
+                ("\\subsection{%s}" . "\\subsection*{%s}")
+                ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+(require 'org-beamer)
+(defvar beamer-opts "
+\\usepackage{listings}
+\\AtBeginSection[]{\\begin{frame}<beamer>\\frametitle{Topic}\\tableofcontents[currentsection]\\end{frame}}
+")
+(defvar my-beamer-headers
+  (concat beamer-opts zh-preamble))
+
+(defadvice org-beamer-amend-header (before insert-my-beamer-headers
+                                           ()
+                                           activate)
+  (setq org-beamer-header-extra
+        (concat my-beamer-headers org-beamer-header-extra)))
 
